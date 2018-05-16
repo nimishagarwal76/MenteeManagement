51112145388bb702e86b2ae2e97f2c203f081a1c
Nimish Agrawal <nimishagarwal76@gmail.com>
2018-05-16 15:12:51 +0530

    last

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function (){
 const lis=document.querySelector('#menteelist ul');
 const list=document.querySelectorAll('#menteelist li');
-console.log(list);
 var mentee=[];
 var ed=0;
 var p;
@@ -25,7 +24,7 @@ function init(){
 }
 
 
-
+// what should happen on pressing save button
 function pressSave(e){
    var addBtn = e.target.parentElement;
    var addhide = addBtn.querySelector('.addbutton');
@@ -39,81 +38,60 @@ function pressSave(e){
    var rt=addBtn.querySelector('.input-r').value;
    var ct=addBtn.querySelector('.input-c').value;
    var rl=addBtn.querySelector('.input-roll').value;
+
+   // getting data entered in form and used to prepare a li element
    prepare(nm,rt,ct,rl);
 
-  
+   // creating an object then pushing it to mentee array and storing in localStorage  
    var stuObj={"name":nm,"rate":rt,"comment":ct,"roll":rl};
-  console.log("dsf",stuObj);
-   console.log(mentee);
    mentee.push(stuObj);
-
    localStorage.menteesRecord=JSON.stringify(mentee);
-// console.log(mentee.length);
 
 
-// 
-	// console.log("ds");
-sshow.style.display='none';
- saddhide.style.display='block';
- 
- 
-
-
-//creating a student object
-
-
-
-
-
-
-addBtn.querySelector('.input-n').value="";
-addBtn.querySelector('.input-r').value="";
-addBtn.querySelector('.input-c').value="";
-addBtn.querySelector('.input-roll').value="";
+   // hiding the form after clicking save button 
+   sshow.style.display='none';
+   saddhide.style.display='block';
+   
+   // making values of form empty again for next entry
+   addBtn.querySelector('.input-n').value="";
+   addBtn.querySelector('.input-r').value="";
+   addBtn.querySelector('.input-c').value="";
+   addBtn.querySelector('.input-roll').value="";
 }
 
 
 
+// to remove an element which is clicked
 
-function remove(e){
+function remove(e)
+{
+// removing from page
+  var li = e.target.parentElement;
+  var nom =li.querySelector(".name");
+  li.parentNode.removeChild(li);
 
-console.log("removeeeeeeeeee");
-var li = e.target.parentElement;
+// removing from local storage 
+  mentee=JSON.parse(localStorage.menteesRecord);
 
-var nom =li.querySelector(".name");
-    
-	li.parentNode.removeChild(li);
-
- mentee=JSON.parse(localStorage.menteesRecord);
-
-for (var i =0; i< mentee.length; i++) {
+  for (var i =0; i< mentee.length; i++) 
+  {
     var items = mentee[i];
-    if (items.name == nom.textContent) {
+    if (items.name == nom.textContent) 
+    {
         mentee.splice(i, 1);
     }
+  }
+
+localStorage.menteesRecord=JSON.stringify(mentee);		
+
 }
 
-localStorage.menteesRecord=JSON.stringify(mentee);
-console.log("removeee",mentee);		
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+// making an object with given details 
 function prepare(nm,rt,ct,rl)
 {
+
 	
-console.log("prepare",nm);
+// creating elements;
 	const cli=document.createElement('li');
 	const cname=document.createElement('span');
 	const cdelete=document.createElement('span');
@@ -126,6 +104,35 @@ console.log("prepare",nm);
 	const cp=document.createElement('span');
 	const croll=document.createElement('span');
 
+
+// backgroundColor of li elements
+var r,g,o = 0;
+    if(rt == 10)
+    {
+    	g=255;
+    	r=0;
+    	o=1
+    }
+	else if(rt >= 5)
+	{
+		g = 255;
+		r = 0;
+		o = 0.2 * (rt % 5);
+	}
+	else if(rt < 5)
+	{
+		g = 0;
+		r = 255;
+		o = 1 - 0.2 * (rt % 5);
+	}
+	
+
+    cli.style.background= "rgba(" +[r,g,0,o].join(',')+")";;
+
+
+
+
+ // addind child to elements
 	cli.appendChild(cname);
 	cli.appendChild(cdelete);
 	cli.appendChild(cedit);
@@ -136,10 +143,10 @@ console.log("prepare",nm);
 	cli.appendChild(cbr2);
 	cli.appendChild(croll);
 	cli.appendChild(ccomment);
-// 	cli.appendChild(cbr);
 	
 	lis.appendChild(cli);  
 
+// adding text content to elements
 	cname.textContent=nm;
 	cdelete.textContent="DELETE";
 	cedit.textContent="EDIT";
@@ -149,7 +156,7 @@ console.log("prepare",nm);
 	cdown.textContent="▼"
 	cp.textContent="RATING: "
 
-
+// adding classes to elements
 	cname.classList.add('name');
 	cdelete.classList.add('delete');
 	cedit.classList.add('edit');
@@ -162,106 +169,99 @@ console.log("prepare",nm);
 
 }
 
+// sorts array by rate value
 function sortArray()
 {
-mentee=JSON.parse(localStorage.menteesRecord);
-mentee.sort(function compare(c,d)
+  mentee=JSON.parse(localStorage.menteesRecord);
+  mentee.sort(function compare(c,d)
 {
-	if (c.rate>d.rate) 
+	if ((c.rate-1)>(d.rate-1)) 
 	{
 		return -1;
 	}
-	else if(c.rate<d.rate)
-		{return 1;}
+	else if((c.rate-1)<(d.rate-1))
+		{
+			return 1;
+		}
 	else{
 		return 0;
 	}
 
 });
-// console.log(mentee);
+
+//storing new sorted array and then printing
 localStorage.menteesRecord=JSON.stringify(mentee);
 for(var i=0;i<mentee.length;++i)
   {
-  	var temp=mentee[i];
+  var temp=mentee[i];
   prepare(temp.name,temp.rate,temp.comment,temp.roll);
-
-
   }
-	
-
-console.log("imhesre");
 
 }
 
+// adding eventlistner to sort button
 
 var sortbtn=document.querySelector('#sort');
+
 sortbtn.addEventListener('click',function(e){
-e.preventDefault();
-lis.innerHTML="";
-sortArray();
-
-
-
-
+  e.preventDefault();
+  lis.innerHTML="";
+  sortArray();
 
 })
 
 
-
+// adding event listner to delete,edit and down buttons 
 lis.addEventListener('click',function(e){
 	if(e.target.className=='delete')
 	{
 		remove(e);
-
-}
+    }
 
 
 	else if(e.target.className=='down')
 	{
-				const sel=e.target.parentElement;
+		const sel=e.target.parentElement;
 		var lists=document.querySelectorAll('.collapse');
-		// console.log(j);
-		
 		var comp=sel.querySelector('.collapse');
-	if(comp.style.display == 'block')
-    {
-      comp.style.display = 'none';
-    }
-    else{
-      Array.from(lists).forEach(function(l){
-           l.style.display="none";
 
-
+		if(comp.style.display == 'block')
+    	{
+      		comp.style.display = 'none';
+    	}
+    	else
+    	{
+      		Array.from(lists).forEach(function(l){
+            l.style.display="none";
 		})
 
-
-      comp.style.display = 'block';
+    	comp.style.display = 'block';
     }
 	}
     
     else if(e.target.className=='edit')
     {
     	
- var foredit =e.target.parentElement;
- var saddhide=document.querySelector('.addbutton');
-   var sshow = document.querySelector('#hide');
-   saddhide.style.display='none';
-sshow.style.display='block';
+		var foredit =e.target.parentElement;
+ 		var saddhide=document.querySelector('.addbutton');
+   		var sshow = document.querySelector('#hide');
+   		saddhide.style.display='none';
+		sshow.style.display='block';
+        window.scrollTo(0,document.body.scrollHeight);
+
+		var nm=foredit.querySelector('.name').textContent;
+		var rt=foredit.querySelector('.rating').textContent;
+		var rl=foredit.querySelector('.rollno').textContent;
+		var ct=foredit.querySelector('.collapse').textContent;
 
 
-var nm=foredit.querySelector('.name').textContent;
-var rt=foredit.querySelector('.rating').textContent;
-var rl=foredit.querySelector('.rollno').textContent;
-var ct=foredit.querySelector('.collapse').textContent;
-// console.log(a.textContent);
 
+		document.querySelector('.input-n').value=nm;
+		document.querySelector('.input-r').value=rt;
+		document.querySelector('.input-c').value=ct;
+		document.querySelector('.input-roll').value=rl;
 
-document.querySelector('.input-n').value=nm;
-document.querySelector('.input-r').value=rt;
-document.querySelector('.input-c').value=ct;
-document.querySelector('.input-roll').value=rl;
-
-remove(e);
+		remove(e);
 
 
 
@@ -269,50 +269,32 @@ remove(e);
     }
 
 })
-// var eform=document.querySelector('#editform');
-// eform.addEventListener('click',function(e){
-//    e.preventDefault();
-//    if(e.target.className='esavebutton')
-//    {
-//    	// console.log(p.target.parentElement);
-//    	var ab=p.target.parentElement;
-//    // ab.querySelector(".name").textContent=eform.querySelector('.einput-n').value;
-//    var c =eform.querySelector('.einput-n');
-//    console.log(c.value);
-
-//    }
 
 
 
-
-// })
-
-
-
+// adding event listner to form for adding elements
 var addForm=document.querySelector('#add');
 addForm.addEventListener('click',function(e){
 	
 	e.preventDefault();
-  var addBtn = e.target.parentElement;
-  var addhide = addBtn.querySelector('.addbutton');
-   var show = addBtn.querySelector('#hide');
-   // var forsave =e.target.parentElement.parentElement;
-   
+  	var addBtn = e.target.parentElement;
+  	var addhide = addBtn.querySelector('.addbutton');
+   	var show = addBtn.querySelector('#hide');
+    
  
+// if add button is pressed changing the display
+  	if(e.target.className=='addbutton')
+  	{
+  		addhide.style.display='none';
+ 		show.style.display='block';
+ 		window.scrollTo(0,document.body.scrollHeight);
+ 	}
 
-  if(e.target.className=='addbutton')
-  {
-  	addhide.style.display='none';
- 
-  show.style.display='block';}
-
-  
-
-
-if(e.target.className=='savebutton')
- {
-pressSave(e);
-}
+// when save is pressed
+	if(e.target.className=='savebutton')
+	{
+		pressSave(e);
+	}
 
 })
 
